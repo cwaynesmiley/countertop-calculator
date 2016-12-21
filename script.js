ed4c174e87981d823098784445791656544ab902
Wayne Smiley <cwaynesmiley@gmail.com>
2016-12-21 16:02:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+var Saljan = 69;
+var Karlby = 129;
+var Hammarp = 149;
+var Ekbacken = 79;
+var Barkaboda = 229;
 
+var totalCost = function(footage, material) {
+  var area = document.getElementById("area").value;
+  var material = document.getElementById("material").value;
+  var cost = footage * material;
+}
+
+console.log(totalCost(footage, material));
+
+//function totalCost(footage, material) {
+//  var cost = (footage * material);  
+//  return cost;
+//}
+
+//$(document).ready(function() {
+//   $("#submitButton").click(function(){
+ //    alert("$cost");
+ //  });
+//});
